f2c64b8769b66ea03121d62a0bbe21d2c5a676d7
EMEA\Victoria.Kush <Victoria.Kush@akqa.com>
2023-02-15 10:44:29 +0100

    Updates

--- precache-manifest.48ef4878ce07f385dbd8958267b2f9fb.js
+++ precache-manifest.48ef4878ce07f385dbd8958267b2f9fb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0597af52f379f5593124fe2eaefbc846",
+    "revision": "a2ad925be9de461ef835f63c37602410",
     "url": "/github-user-seacher-app/index.html"
   },
   {
-    "revision": "de95a5662ecebf8b4d33",
+    "revision": "936bf4e59bf053ae9aa3",
     "url": "/github-user-seacher-app/static/css/main.dee56b73.chunk.css"
   },
   {
-    "revision": "6db29c9d6ff91689bfad",
-    "url": "/github-user-seacher-app/static/js/2.6a13e593.chunk.js"
+    "revision": "63b42d493bacce7e159a",
+    "url": "/github-user-seacher-app/static/js/2.b4505669.chunk.js"
   },
   {
     "revision": "320addaff3e37295a01ab28fa4b66477",
-    "url": "/github-user-seacher-app/static/js/2.6a13e593.chunk.js.LICENSE.txt"
+    "url": "/github-user-seacher-app/static/js/2.b4505669.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "de95a5662ecebf8b4d33",
-    "url": "/github-user-seacher-app/static/js/main.975d088c.chunk.js"
+    "revision": "936bf4e59bf053ae9aa3",
+    "url": "/github-user-seacher-app/static/js/main.bdfe2657.chunk.js"
   },
   {
     "revision": "9b24096ba0fdd342367f",
