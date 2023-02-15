d81ba0e136a3299fbbbc8e42d5f3cabf5a24ff35
EMEA\Victoria.Kush <Victoria.Kush@akqa.com>
2023-02-15 10:28:23 +0100

    Updates

---
+++ precache-manifest.aaa2860f972755dc8da9b44bc116bd76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0597af52f379f5593124fe2eaefbc846",
+    "url": "/github-user-seacher-app/index.html"
+  },
+  {
+    "revision": "de95a5662ecebf8b4d33",
+    "url": "/github-user-seacher-app/static/css/main.dee56b73.chunk.css"
+  },
+  {
+    "revision": "6db29c9d6ff91689bfad",
+    "url": "/github-user-seacher-app/static/js/2.6a13e593.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/github-user-seacher-app/static/js/2.6a13e593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de95a5662ecebf8b4d33",
+    "url": "/github-user-seacher-app/static/js/main.975d088c.chunk.js"
+  },
+  {
+    "revision": "9b24096ba0fdd342367f",
+    "url": "/github-user-seacher-app/static/js/runtime-main.db3fbe81.js"
+  },
+  {
+    "revision": "5d292e21532ea5857531cdb8b0180637",
+    "url": "/github-user-seacher-app/static/media/Octocat.5d292e21.jpg"
+  }
+]);
